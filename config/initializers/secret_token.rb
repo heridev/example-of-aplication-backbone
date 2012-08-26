a37bdeed6a263678f38ce40b84682bf636b8eac3
heriberto perez <heriberto.perez@crowdint.com>
2012-08-26 18:43:52 -0500

    initialize the backbone app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneApp::Application.config.secret_token = '69578f5b9db515c25a06d240af4e5aabf089fca5d456e041162bd878352de75ac7eb91dfb05741663c2e9d15b5c8f72deff0b4b6bf6054263b9c30db834e0df9'
